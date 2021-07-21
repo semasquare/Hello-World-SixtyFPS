d64634b8e56988f968af9d0ef15d1fbefeb23f57
Fabian Wienand <fabian.wienand@semasquare.com>
2021-07-21 12:41:35 +0200

    initial commit

---
+++ target/debug/deps/hello_world-4ee361154ff21fff.d
@@ -0,0 +1,5 @@
+/home/fabian/semasquare/yocto-images/hello_world/target/debug/deps/hello_world-4ee361154ff21fff: src/main.rs
+
+/home/fabian/semasquare/yocto-images/hello_world/target/debug/deps/hello_world-4ee361154ff21fff.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/fabian/semasquare/yocto-images/hello_world/target/debug/hello_world: /home/fabian/semasquare/yocto-images/hello_world/src/main.rs
